f54542b2ce02d0c9c23b800cbb7ea484bef4e6da
xsjad0 <marius.schenzle@googlemail.com>
2019-12-31 12:20:26 +0100

    refactore gesturelistener

--- app/src/main/java/dev/gui/imageflinger/ui/GestureListener.java
+++ app/src/main/java/dev/gui/imageflinger/ui/GestureListener.java
@@ -10,12 +10,11 @@ import dev.gui.imageflinger.ui.camera.CameraViewModel;
 import dev.gui.imageflinger.ui.telegram.TelegramViewModel;
 import dev.gui.imageflinger.ui.screenshots.ScreenshotViewModel;
 
-
 public class GestureListener extends SimpleOnGestureListener {
 
-    final int SWIPE_MIN_DISTANCE = 120;
-    final int SWIPE_MAX_OFF_PATH = 250;
-    final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private final int SWIPE_MIN_DISTANCE = 120;
+    private final int SWIPE_MAX_OFF_PATH = 250;
+    private final int SWIPE_THRESHOLD_VELOCITY = 200;
     private final String DEBUG_TAG = "GestureListener";
     private ViewModel viewModel;
 
@@ -35,12 +34,10 @@ public class GestureListener extends SimpleOnGestureListener {
         try {
             if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
                 return false;
-            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
                 Log.e(DEBUG_TAG, "Right to Left");
                 action(Gestures.RIGHT_TO_LEFT.ordinal());
-            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
                 Log.e(DEBUG_TAG, "Left to Right");
                 action(Gestures.LEFT_TO_RIGHT.ordinal());
             }
@@ -50,16 +47,14 @@ public class GestureListener extends SimpleOnGestureListener {
         return super.onFling(e1, e2, velocityX, velocityY);
     }
 
-    private void action(Integer direction) {
+    private void action(int direction) {
         if (viewModel instanceof CameraViewModel) {
             CameraViewModel vm = (CameraViewModel) viewModel;
             vm.next(direction);
-        }
-        else if (viewModel instanceof TelegramViewModel) {
+        } else if (viewModel instanceof TelegramViewModel) {
             TelegramViewModel vm = (TelegramViewModel) viewModel;
             vm.next(direction);
-        }
-        else {
+        } else {
             ScreenshotViewModel vm = (ScreenshotViewModel) viewModel;
             vm.next(direction);
         }
